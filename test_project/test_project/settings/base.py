23d53aaa1e18c7f9f8a9efd46f9be8ed7e2fa199
Juergen Brendel <jbrendel@cisco.com>
2014-11-06 23:47:29 +1300

    Removed the staticfile app, not needed right now.

--- test_project/test_project/settings/base.py
+++ test_project/test_project/settings/base.py
@@ -202,7 +202,7 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    #'django.contrib.staticfiles',
     # Some of our own apps
     'django_extensions'
 )
